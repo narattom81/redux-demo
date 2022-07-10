528262a110a6a6f59cc5f42c159d99bb9ff790d1
narattom <narattom81@gmail.com>
2022-07-11 04:48:28 +0800

    Initial index.js file

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log('from index.js')
+const redux = require("redux");
+
+const BUY_CAKE = 'BUY_CAKE'
+const initialStore = {
+    cakeCount: 10,
+    cakeType: 'blueberry'
+}
+
+const blubberyCakeReducer = (state = initialStore, action) => {
+    if (typeof action == 'undefined') {
+        return {...state}
+    }
+    switch (action.type) {
+        case BUY_CAKE:
+            return {...state, cakeCount: state.cakeCount - 1}
+        default:
+            return {...state}
+    }
+}
+
+const appleCakeReducer = (state = {cakeCount: 15, cakeType: 'apple'}, action) => {
+    switch (action.type) {
+        case BUY_CAKE:
+            return {...state, cakeCount: state.cakeCount - 1}
+        default:
+            return {...state}
+    }
+}
+const combinedReducer = redux.combineReducers({appleCakeReducer, blubberyCakeReducer})
+const store = redux.legacy_createStore(combinedReducer)
+
+const actionCreator = () => {
+    return {
+        type: BUY_CAKE
+    }
+}
+const unsubscribe = store.subscribe(() => {
+    console.log('remaining count:', store.getState())
+})
+console.log('initial count',store.getState())
+store.dispatch(actionCreator())
+store.dispatch(actionCreator())
+store.dispatch(actionCreator())
+
+unsubscribe()
